2c0f4d82352defd78f424da12da726c49fdad584
Tarun Bansal <bansal.tarun26@gmail.com>
2014-02-17 02:06:52 +0530

    Third commit

--- url/insert.php
+++ url/insert.php
@@ -13,23 +13,35 @@ $s[1]=chr(ord($s[1])+1);
 return $s;
 }
 $uri = $_POST['URL'];
-echo $uri;
+// "urishortner" is name of database
+//connecting to database named urishortner
 $db = mysqli_connect('127.0.0.1','root','ttt','urishortner');
-if($db)
+if(!$db)
 {
-echo "correctly connected to Database urishortner";
+echo "Connection error to Database urishortner";
 }
+
+// first url is the url of website to be shortened
+// second url is the table name
+// code is the unique alphabet code for every website
+
+
+// here it fetches the last code assignned to the recently registered website
 $initcode = "SELECT `url` FROM `url` WHERE `code` = 'lastcode'";
 $result1 = mysqli_query($db,$initcode);
 $row1 = mysqli_fetch_array($result1);
 $code = newcode($row1['url']);
+
+// updating the last assigned code to the new value for future refrence
 $result2 = mysqli_query($db,"UPDATE `url` SET `url` = '$code' WHERE `code` = 'lastcode'");
+
+// inserting the newly registered website and the code generated for it
 $query = "INSERT INTO `url`(`code`, `url`) VALUES ('$code','$uri')";
 $result = mysqli_query($db,$query);
 
 if($result)
 {
-echo 'Your new url is: <a href="http://localhost/url/new1.php?i='.$code.'">http://localhost/url/new1.php?i='.$code.'</a>';
+echo 'Your new url is: <a href="http://localhost/url/j/'.$code.'">http://localhost/url/j/'.$code.'</a>';
 }
 else{echo "error";}
 
